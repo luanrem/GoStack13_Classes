a356400bfc81d598c522f646861d5411f23eca0e
luanrem <luanrem@gmail.com>
2020-09-13 16:42:16 -0300

    Estilos globais

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 // import { Container } from './styles';
 
-const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-};
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
