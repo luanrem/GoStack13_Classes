9e37021c26b144189b0a15c66239742a06a24745
luanrem <luanrem@gmail.com>
2020-09-13 19:18:55 -0300

    Utilizando Unform

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+// import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
-
-import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
