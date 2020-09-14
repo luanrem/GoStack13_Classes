c9e57155ff371998926b1eda096bb6aa6a338610
luanrem <luanrem@gmail.com>
2020-09-14 18:40:37 -0300

    Inserindo modulo 4

---
+++ 04-iniciando-back-end/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'f56cc10361168167f644d0a43c8e9f87',
+        expiresIn: '1d',
+    }
+}
---
+++ 04-iniciando-back-end/src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm'
+
+import ensureAuthenticaded from '../middlewares/ensureAuthenticated';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticaded);
+
+
+appointmentsRouter.get('/', async (request, response) => {
+    console.log(request.user)
+
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+    const appointments = await appointmentsRepository.find();
+
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', async (request, response) => {
+    const { provider_id, date } = request.body;
+
+    const parsedDate = parseISO(date);
+
+    const CreateAppointment = new CreateAppointmentService();
+
+    const appointment = await CreateAppointment.execute({
+        date: parsedDate,
+        provider_id,
+    });
+
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
