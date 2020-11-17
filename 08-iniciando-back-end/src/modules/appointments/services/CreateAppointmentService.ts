4a6d73ab32fe1283806958c1a530eb916001838f
luanrem <luanrem@gmail.com>
2020-11-17 13:07:43 -0300

    Dependency Inversion Principle

--- 08-iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
+++ 08-iniciando-back-end/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,11 +1,10 @@
 import { startOfHour } from 'date-fns';
 
-import { getCustomRepository } from 'typeorm';
-
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
+import IAppointmentsRepository from '../repositories/IAppointmentesRepository';
+// import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 /**
  * [x]Recebimento das informações
@@ -13,7 +12,7 @@ import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRe
  * [ ]Acesso ao repositório
  */
 
-interface RequestDTO {
+interface IRequestDTO {
   provider_id: string;
   date: Date;
 }
@@ -23,15 +22,23 @@ interface RequestDTO {
  */
 
 class CreateAppointmentService {
+  constructor(
+    // Se eu coloco private a variável já é criada antes de ser puxada aqui
+    // É como se estivesse colocando antes do constructor o seguinte codigo:
+    // private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {
+
+  }
+
   public async execute({
     date,
     provider_id,
-  }: RequestDTO): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  }: IRequestDTO): Promise<Appointment> {
 
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -39,7 +46,7 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked');
     }
 
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
