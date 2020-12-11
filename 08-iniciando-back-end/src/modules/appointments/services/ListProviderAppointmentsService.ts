27d65ce07cc56b7e3c647f2ff30e9f3f5699c127
luanrem <luanrem@gmail.com>
2020-12-11 16:42:23 -0300

    Serializacao no cache

--- 08-iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ 08-iniciando-back-end/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { injectable, inject } from 'tsyringe';
 
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
@@ -47,7 +48,7 @@ class ListProviderAppointmentsService {
         },
       );
 
-      await this.cacheProvider.save(cacheKey, appointments);
+      await this.cacheProvider.save(cacheKey, classToClass(appointments));
     }
 
     return appointments;
