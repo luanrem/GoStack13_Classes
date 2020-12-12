17bafb7752a6b05c18df60330ba91d52f5094f5f
luanrem <luanrem@gmail.com>
2020-12-12 15:45:35 -0300

    pagina de perfil

--- 09-gobarber-web/src/pages/Dashboard/index.tsx
+++ 09-gobarber-web/src/pages/Dashboard/index.tsx
@@ -3,9 +3,12 @@ import { isToday, format, isAfter } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
+import { Link } from 'react-router-dom';
+import { parseISO } from 'date-fns/esm';
 
 import { FiClock, FiPower } from 'react-icons/fi';
-import { parseISO } from 'date-fns/esm';
+import { useAuth } from '../../hooks/auth';
+
 import {
   Container,
   Header,
@@ -19,9 +22,8 @@ import {
   Appointment,
 } from './styles';
 
-import logoImg from '../../assets/logo.svg';
-import { useAuth } from '../../hooks/auth';
 import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
 
 interface MonthAvailabilityItem {
   day: number;
@@ -144,7 +146,9 @@ const Dashboard: React.FC = () => {
             <img src={user.avatar_url} alt={user.name} />
             <div>
               <span>Bem-vindo,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
